065ce05be5c4782126acea5de3f46bf052d022ce
cristobalsalazar <chris.salza@gmail.com>
2023-11-17 17:02:34 -0700

    update testst

--- migra_test.go
+++ migra_test.go
@@ -3,60 +3,80 @@ package migra_test
 import (
 	"context"
 	"database/sql"
-	"io/fs"
-	"os"
 	"testing"
 
 	"github.com/cristosa/migra"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func TestMigrateUp(t *testing.T) {
-	ctx := context.Background()
-	db, err := sql.Open("pgx", os.Getenv("CONNECTION_STRING"))
+var ctx = context.Background()
+var connectionString = "postgres://migra:migra@localhost:5432/migra"
+
+func initMigra(t *testing.T) *migra.Migra {
+	db, err := sql.Open("pgx", connectionString)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := migra.CreateMigrationTable(ctx, db); err != nil {
+	m := migra.New(db)
+
+	if err := m.CreateMigrationTable(ctx); err != nil {
 		t.Fatal(err)
 	}
 
 	t.Cleanup(func() {
-		migra.DropMigrationTable(ctx, db)
+		m.DropMigrationTable(ctx)
 	})
 
-	// ok we point to the directory
-	fs := os.DirFS("sql_test").(fs.ReadDirFS)
-	m := migra.New(db, fs)
+	return m
+}
 
-	if err := m.Up(ctx, ""); err != nil {
-		t.Fatal(err)
+func TestMigrateUp(t *testing.T) {
+	m := initMigra(t)
+	migrations := []migra.Migration{
+		{
+			Name:        "Test Users",
+			Description: "Creates test users table with username and password fields",
+			Up: `CREATE TABLE test_users (
+			id SERIAL PRIMARY KEY,
+			username VARCHAR(255) NOT NULL UNIQUE,
+			password VARCHAR(1024) NOT NULL
+		);`,
+			Down: `DROP TABLE test_users;`,
+		},
+		{
+			Name:        "Default User",
+			Description: "Adds a default test user",
+			Up:          "INSERT INTO test_users (username, password) VALUES ('foo', 'bar')",
+			Down:        "DELETE FROM test_users WHERE username = 'foo'",
+		},
 	}
 
-	migrations, err := m.ListMigrations(ctx)
+	for i := range migrations {
+		if err := m.Push(ctx, &migrations[i]); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	found, err := m.ListMigrations(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(migrations) != 1 {
-		t.Fatal("expected 1 migration")
+	if len(found) != len(migrations) {
+		t.Fatalf("expected %d migrations, got %d", len(migrations), len(found))
 	}
 }
 
 func TestInitMigrationTables(t *testing.T) {
-	ctx := context.Background()
-	db, err := sql.Open("pgx", os.Getenv("CONNECTION_STRING"))
-	if err != nil {
-		t.Fatal(err)
-	}
+	m := initMigra(t)
 
-	if err := migra.CreateMigrationTable(ctx, db); err != nil {
+	if err := m.CreateMigrationTable(ctx); err != nil {
 		t.Fatal(err)
 	}
 
 	t.Cleanup(func() {
-		migra.DropMigrationTable(ctx, db)
+		m.DropMigrationTable(ctx)
 	})
 }
