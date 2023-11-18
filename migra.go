68eb539ddc484e4611e2ba8e98d6dbc94fe25d5b
cristobalsalazar <chris.salza@gmail.com>
2023-11-17 17:02:24 -0700

    api based migrations

--- migra.go
+++ migra.go
@@ -3,25 +3,83 @@ package migra
 import (
 	"context"
 	"database/sql"
-	"io"
-	"io/fs"
+	"fmt"
 	"time"
 )
 
+const DefaultMigrationTable = "_migra"
+
 type Migration struct {
-	ID         int64
-	Filename   string
-	MigratedAt time.Time
+	ID          int64
+	Name        string
+	Description string
+	Up          string
+	Down        string
+	Position    int64
+	MigratedAt  time.Time
 }
 
 type Migra struct {
-	*sql.DB
-	fs.ReadDirFS
+	db        *sql.DB
+	tableName string
+}
+
+// New creates a new Migra instance.
+func New(db *sql.DB) *Migra {
+	return &Migra{
+		db:        db,
+		tableName: DefaultMigrationTable,
+	}
+}
+
+// SetMigrationsTable sets the default table where migrations will be stored and executed
+func (m *Migra) SetMigrationsTable(table string) {
+	m.tableName = table
+}
+
+// CreateMigrationTable creates the table where migrations will be stored and executed.
+// The name of the table can be set using the SetMigrationsTable method.
+// Otherwise, the value of DefaultMigrationTable is used.
+func (m *Migra) CreateMigrationTable(ctx context.Context) error {
+	if m.tableName == "" {
+		m.tableName = DefaultMigrationTable
+	}
+
+	_, err := m.db.ExecContext(ctx, fmt.Sprintf(`create table if not exists %s (
+		id serial primary key,
+		name varchar(255) not null unique,
+		description text,
+		up text,
+		down text,
+		position serial not null,
+		migrated_at timestamptz
+	);`, m.tableName))
+
+	return err
+}
+
+// Push adds a migration to the database and executes it
+func (m *Migra) Push(ctx context.Context, migration *Migration) error {
+	tx, err := m.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if err := m.insertMigrationTx(ctx, tx, migration); err != nil {
+		return err
+	}
+
+	if err := m.upMigrationTx(ctx, tx, migration); err != nil {
+		return err
+	}
+
+	return tx.Commit()
 }
 
 // ListMigrations returns all the executed migrations
 func (m *Migra) ListMigrations(ctx context.Context) ([]Migration, error) {
-	rows, err := m.QueryContext(ctx, "select id, filename, migrated_at from _migra")
+	sql := fmt.Sprintf(`SELECT id, name, description, up, down, position, migrated_at FROM %s ORDER BY position ASC`, m.tableName)
+	rows, err := m.db.QueryContext(ctx, sql)
 
 	if err != nil {
 		return nil, err
@@ -31,84 +89,41 @@ func (m *Migra) ListMigrations(ctx context.Context) ([]Migration, error) {
 	migrations := make([]Migration, 0)
 	for rows.Next() {
 		var migration Migration
-		if err := rows.Scan(&migration.ID, &migration.Filename, &migration.MigratedAt); err != nil {
+		if err := rows.Scan(
+			&migration.ID,
+			&migration.Name,
+			&migration.Description,
+			&migration.Up,
+			&migration.Down,
+			&migration.Position,
+			&migration.MigratedAt); err != nil {
 			return migrations, err
 		}
+
 		migrations = append(migrations, migration)
 	}
 
 	return migrations, nil
 }
 
-// New creates a new Migra instance.
-func New(db *sql.DB, fileSystem fs.ReadDirFS) *Migra {
-	return &Migra{
-		DB:        db,
-		ReadDirFS: fileSystem,
+// DropMigrationTable drops the migrations table
+func (m *Migra) DropMigrationTable(ctx context.Context) error {
+	_, err := m.db.ExecContext(ctx, fmt.Sprintf("drop table %s", m.tableName))
+	return err
+}
+
+func (m *Migra) insertMigrationTx(ctx context.Context, tx *sql.Tx, mig *Migration) error {
+	sql := fmt.Sprintf("INSERT INTO %s (name, description, up, down) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING", m.tableName)
+	_, err := tx.ExecContext(ctx, sql, mig.Name, mig.Description, mig.Up, mig.Down)
+	return err
+}
+
+func (m *Migra) upMigrationTx(ctx context.Context, tx *sql.Tx, mig *Migration) error {
+	if _, err := tx.ExecContext(ctx, mig.Up); err != nil {
+		return err
 	}
-}
 
-// CreateMigrationTable initializes the database with the migrations table
-func CreateMigrationTable(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `create table if not exists _migra(
-		id serial primary key,
-		filename varchar(255) not null,
-		migrated_at timestamptz not null default now()
-	);`)
-
+	sql := fmt.Sprintf("update %s set migrated_at = now() where name = $1", m.tableName)
+	_, err := tx.ExecContext(ctx, sql, mig.Name)
 	return err
-}
-
-func DropMigrationTable(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, "drop table _migra")
-	return err
-}
-
-// Up migrates files up to the given filename.
-func (m *Migra) Up(ctx context.Context, filename string) error {
-	return fs.WalkDir(m.ReadDirFS, ".", func(path string, entry fs.DirEntry, err error) error {
-		if entry.IsDir() {
-			return nil
-		}
-
-		f, err := m.ReadDirFS.Open(path)
-		if err != nil {
-			return err
-		}
-
-		defer f.Close()
-
-		content, err := io.ReadAll(f)
-		if err != nil {
-			return err
-		}
-
-		// we need to execute in transaction
-		tx, err := m.DB.Begin()
-		if err != nil {
-			return err
-		}
-
-		defer tx.Rollback()
-
-		// execute the file
-		if _, err := tx.ExecContext(ctx, string(content)); err != nil {
-			return err
-		}
-
-		if _, err := tx.ExecContext(ctx, `insert into _migra (filename) values ($1)`, entry.Name()); err != nil {
-			return err
-		}
-
-		if err := tx.Commit(); err != nil {
-			return err
-		}
-
-		// here we weould break
-		if entry.Name() == filename {
-			return nil
-		}
-
-		return nil
-	})
 }
